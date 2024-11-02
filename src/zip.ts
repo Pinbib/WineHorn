9390b27d7e6b565d4a31801a0b039652af60d847
Pinbib <pancenkoe353@gmail.com>
2024-11-02 13:41:39 +0000

    a function was created to compress text files into one text file

--- src/zip.ts
+++ src/zip.ts
@@ -1,4 +1,5 @@
-import {gzipSync, gunzipSync} from "zlib";
+import {gzipSync, gunzipSync} from "node:zlib";
+import {readFileSync, writeFile} from "node:fs";
 
 export function compress(input: string): string {
 	return gzipSync(input).toString("base64");
@@ -6,4 +7,21 @@ export function compress(input: string): string {
 
 export function decompress(input: string): string {
 	return gunzipSync(Buffer.from(input, "base64")).toString();
+}
+
+export function filesCompress(out: string, files: string[]): Promise<any> {
+	return new Promise((resolve, reject) => {
+		try {
+			let file: string[] = [];
+			files.forEach(path => {
+				let data = decompress(readFileSync(path, "utf-8"));
+				file.push(`\n\n${data}`);
+			});
+
+			let data: string = compress(file.join());
+			writeFile(out, data, resolve);
+		} catch (err) {
+			if (err) reject(err);
+		}
+	});
 }
